293516fc8d64582ec0ea029c6498af03abafc106
SkHCrusher <tobiasstewen@cheater2.de>
2017-08-10 14:44:21 +0200

    Cleanup

--- Infinitag_Core.cpp
+++ Infinitag_Core.cpp
@@ -17,189 +17,189 @@ Infinitag_Core::Infinitag_Core()
 {
 }
 
-unsigned long Infinitag_Core::ir_encode(bool isSystem, unsigned int gameId, unsigned int teamId, unsigned int playerId, unsigned int cmd, unsigned int cmdValue){
-	int tmpBits[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	unsigned long finalVal = 0;
-	byte x;
+unsigned long Infinitag_Core::irEncode(bool isSystem, unsigned int gameId, unsigned int teamId, unsigned int playerId, unsigned int cmd, unsigned int cmdValue){
+  int tmpBits[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  unsigned long finalVal = 0;
+  byte x;
   
-	// System
-	tmpBits[0] = (isSystem == true)?1:0;
+  // System
+  tmpBits[0] = (isSystem == true)?1:0;
 
-	// GameId	
-	x = gameId; 
-	for(int i = 0; i < 2;i++){
-		tmpBits[(2 - i)] = bitRead(x, i);
-	}
+  // GameId  
+  x = gameId; 
+  for(int i = 0; i < 2;i++){
+    tmpBits[(2 - i)] = bitRead(x, i);
+  }
   
-	// TeamId
-	x = teamId; 
-	for(int i = 0; i < 3;i++){
-		tmpBits[2 + (3 - i)] = bitRead(x, i);
-	}
+  // TeamId
+  x = teamId; 
+  for(int i = 0; i < 3;i++){
+    tmpBits[2 + (3 - i)] = bitRead(x, i);
+  }
 
-	// PlayerId
-	x = playerId; 
-	for(int i = 0; i < 5;i++){
-		tmpBits[5 + (5 - i)] = bitRead(x, i);
-	}
+  // PlayerId
+  x = playerId; 
+  for(int i = 0; i < 5;i++){
+    tmpBits[5 + (5 - i)] = bitRead(x, i);
+  }
   
-	// Command
-	x = cmd; 
-	for(int i = 0; i < 4;i++){
-		tmpBits[10 + (4 - i)] = bitRead(x, i);
-	}
+  // Command
+  x = cmd; 
+  for(int i = 0; i < 4;i++){
+    tmpBits[10 + (4 - i)] = bitRead(x, i);
+  }
   
-	// Command value
-	x = cmdValue; 
-	for(int i = 0; i < 8;i++){
-		tmpBits[14 + (8 - i)] = bitRead(x, i);
-	}
-	
-	// CheckBit
-	// Still needs to be calculated
-	int checkBits = 0;
-	for(int i = 0; i < 23;i++){
-		if (tmpBits[i] == 1) {
-			checkBits++;
-		}
-	}
-	tmpBits[23] = (checkBits % 2);
+  // Command value
+  x = cmdValue; 
+  for(int i = 0; i < 8;i++){
+    tmpBits[14 + (8 - i)] = bitRead(x, i);
+  }
   
-	return bits_to_long(tmpBits);
+  // CheckBit
+  // Still needs to be calculated
+  int checkBits = 0;
+  for(int i = 0; i < 23;i++){
+    if (tmpBits[i] == 1) {
+      checkBits++;
+    }
+  }
+  tmpBits[23] = (checkBits % 2);
+  
+  return bitsToLong(tmpBits);
 }
 
-unsigned long Infinitag_Core::bits_to_long(int *tmpBits) {
-	unsigned long result = 0;
-	
-	for(int i = 0; i < 24;i++){
-		// + 0.49 due to pow and float
-		result += round(tmpBits[24 - i - 1] * (pow(2, i) + 0.49));
-	}
-	
-	return result;
+unsigned long Infinitag_Core::bitsToLong(int *tmpBits) {
+  unsigned long result = 0;
+  
+  for(int i = 0; i < 24;i++){
+    // + 0.49 due to pow and float
+    result += round(tmpBits[24 - i - 1] * (pow(2, i) + 0.49));
+  }
+  
+  return result;
 }
 
-void Infinitag_Core::ir_to_bytes(unsigned long code, byte *result) {
-	if(code > 16777215) {
-		return false;
-	}
-	
-	int tmpBits[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	int checkBits = 0;
-	
-	for(int i = 0; i < 24;i++){
-		tmpBits[i] =  bitRead(code, 23 - i);
-		if (i < 23 && tmpBits[i] == 1) {
-			checkBits++;
-		}
-	}
-	
-	
-	// Checkbit check
-	if (checkBits % 2 != tmpBits[23]) {
-		return false;
-	}
-	
-	//alternate: r1 | (r2 << 1) | (r3 << 2) | (r4 << 3) | (r5 << 4);
-	char byte_one = (tmpBits[0] << 7) | (tmpBits[1] << 6) | (tmpBits[2] << 5) | (tmpBits[3] << 4) | (tmpBits[4] << 3) | (tmpBits[5] << 2) | (tmpBits[6] << 1) | tmpBits[7];
-	char byte_two = (tmpBits[8] << 7) | (tmpBits[9] << 6) | (tmpBits[10] << 5) | (tmpBits[11] << 4) | (tmpBits[12] << 3) | (tmpBits[13] << 2) | (tmpBits[14] << 1) | tmpBits[15];
-	char byte_three = (tmpBits[16] << 7) | (tmpBits[17] << 6) | (tmpBits[18] << 5) | (tmpBits[19] << 4) | (tmpBits[20] << 3) | (tmpBits[21] << 2) | (tmpBits[22] << 1) | tmpBits[23];
+void Infinitag_Core::irToBytes(unsigned long code, byte *result) {
+  if(code > 16777215) {
+    return false;
+  }
+  
+  int tmpBits[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  int checkBits = 0;
+  
+  for(int i = 0; i < 24;i++){
+    tmpBits[i] =  bitRead(code, 23 - i);
+    if (i < 23 && tmpBits[i] == 1) {
+      checkBits++;
+    }
+  }
+  
+  
+  // Checkbit check
+  if (checkBits % 2 != tmpBits[23]) {
+    return false;
+  }
+  
+  //alternate: r1 | (r2 << 1) | (r3 << 2) | (r4 << 3) | (r5 << 4);
+  char byteOne = (tmpBits[0] << 7) | (tmpBits[1] << 6) | (tmpBits[2] << 5) | (tmpBits[3] << 4) | (tmpBits[4] << 3) | (tmpBits[5] << 2) | (tmpBits[6] << 1) | tmpBits[7];
+  char byteTwo = (tmpBits[8] << 7) | (tmpBits[9] << 6) | (tmpBits[10] << 5) | (tmpBits[11] << 4) | (tmpBits[12] << 3) | (tmpBits[13] << 2) | (tmpBits[14] << 1) | tmpBits[15];
+  char byteThree = (tmpBits[16] << 7) | (tmpBits[17] << 6) | (tmpBits[18] << 5) | (tmpBits[19] << 4) | (tmpBits[20] << 3) | (tmpBits[21] << 2) | (tmpBits[22] << 1) | tmpBits[23];
 
-	result[0] = byte_one;
-	result[1] = byte_two;
-	result[2] = byte_three;
+  result[0] = byteOne;
+  result[1] = byteTwo;
+  result[2] = byteThree;
 }
 
-void Infinitag_Core::ir_decode(unsigned long code){
-	if(code > 16777215) {
-		return false;
-	}
-	
-	int tmpBits[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	byte x;
-	
-	int checkBits = 0;
-	for(int i = 0; i < 24;i++){
-		tmpBits[i] =  bitRead(code, 23 - i);
-		if (i < 23 && tmpBits[i] == 1) {
-			checkBits++;
-		}
-	}
+void Infinitag_Core::irDecode(unsigned long code){
+  if(code > 16777215) {
+    return false;
+  }
+  
+  int tmpBits[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  byte x;
+  
+  int checkBits = 0;
+  for(int i = 0; i < 24;i++){
+    tmpBits[i] =  bitRead(code, 23 - i);
+    if (i < 23 && tmpBits[i] == 1) {
+      checkBits++;
+    }
+  }
    
    // Checkbit check
    if (checkBits % 2 != tmpBits[23]) {
-	   return false;
+     return false;
    }
    
-	// System
-	ir_recv_is_system = tmpBits[0];
+  // System
+  irRecvIsSystem = tmpBits[0];
   
-	// GameId
-	x = 0;
-	for(int i = 0; i < 2;i++){
-		bitWrite(x, i, tmpBits[(2 - i)]);
-	}
-	ir_recv_game_id = x;
+  // GameId
+  x = 0;
+  for(int i = 0; i < 2;i++){
+    bitWrite(x, i, tmpBits[(2 - i)]);
+  }
+  irRecvGameId = x;
   
-	// TeamId
-	x = 0;
-	for(int i = 0; i < 3;i++){
-		bitWrite(x, i, tmpBits[(5 - i)]);
-	}
-	ir_recv_team_id = x;
+  // TeamId
+  x = 0;
+  for(int i = 0; i < 3;i++){
+    bitWrite(x, i, tmpBits[(5 - i)]);
+  }
+  irRecvTeamId = x;
   
-	// PlayerId
-	x = 0;
-	for(int i = 0; i < 5;i++){
-		bitWrite(x, i, tmpBits[(10 - i)]);
-	}
-	ir_recv_player_id = x;
+  // PlayerId
+  x = 0;
+  for(int i = 0; i < 5;i++){
+    bitWrite(x, i, tmpBits[(10 - i)]);
+  }
+  irRecvPlayerId = x;
   
-	// Command
-	x = 0;
-	for(int i = 0; i < 4;i++){
-		bitWrite(x, i, tmpBits[(14 - i)]);
-	}
-	ir_recv_cmd = x;
+  // Command
+  x = 0;
+  for(int i = 0; i < 4;i++){
+    bitWrite(x, i, tmpBits[(14 - i)]);
+  }
+  irRecvCmd = x;
   
-	// CommandValue
-	x = 0;
-	for(int i = 0; i < 8;i++){
-		bitWrite(x, i, tmpBits[(22 - i)]);
-	}
-	ir_recv_cmd_value = x;
-	
-	// CheckBit
-	x = 0;
-	for(int i = 0; i < 1;i++){
-		bitWrite(x, i, tmpBits[(23 - i)]);
-	}
-	ir_recv_check_bit = x;
+  // CommandValue
+  x = 0;
+  for(int i = 0; i < 8;i++){
+    bitWrite(x, i, tmpBits[(22 - i)]);
+  }
+  irRecvCmdValue = x;
+  
+  // CheckBit
+  x = 0;
+  for(int i = 0; i < 1;i++){
+    bitWrite(x, i, tmpBits[(23 - i)]);
+  }
+  irRecvCheckBit = x;
 }
 
-void Infinitag_Core::ir_decode(byte *data){
-	int tmpBits[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-	
-	for (int i = 0; i < 8; i++) {
-		tmpBits[7 - i] = bitRead(data[1], i);
-	}
-	for (int i = 0; i < 8; i++) {
-		tmpBits[15 - i] = bitRead(data[2], i);
-	}
-	for (int i = 0; i < 8; i++) {
-		tmpBits[23 - i] = bitRead(data[3], i);
-	}
-	
-	ir_decode(bits_to_long(tmpBits));
+void Infinitag_Core::irDecode(byte *data){
+  int tmpBits[24] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  
+  for (int i = 0; i < 8; i++) {
+    tmpBits[7 - i] = bitRead(data[1], i);
+  }
+  for (int i = 0; i < 8; i++) {
+    tmpBits[15 - i] = bitRead(data[2], i);
+  }
+  for (int i = 0; i < 8; i++) {
+    tmpBits[23 - i] = bitRead(data[3], i);
+  }
+  
+  irDecode(bitsToLong(tmpBits));
 }
 
 
-void Infinitag_Core::ir_dump(unsigned long code){
-	for(int i = 0; i < 24;i++){
-		Serial.print(bitRead(code, 23 - i));
-		if (i == 0 || i == 2 || i == 5 || i == 10 || i == 14 || i == 22) {
-			Serial.print("|");
-		}
-	}
-	Serial.println("");
+void Infinitag_Core::irDump(unsigned long code){
+  for(int i = 0; i < 24;i++){
+    Serial.print(bitRead(code, 23 - i));
+    if (i == 0 || i == 2 || i == 5 || i == 10 || i == 14 || i == 22) {
+      Serial.print("|");
+    }
+  }
+  Serial.println("");
 }
--- Infinitag_Core.h
+++ Infinitag_Core.h
@@ -8,31 +8,31 @@
 */
 
 #ifndef Infinitag_Core_h
-	#define Infinitag_Core_h
+  #define Infinitag_Core_h
 
-	#include "Arduino.h"
+  #include "Arduino.h"
 
-	class Infinitag_Core
-	{
-		public:
-			Infinitag_Core();
-	
-			unsigned long ir_encode(bool isSystem, unsigned int gameId, unsigned int teamId, unsigned int playerId, unsigned int cmd, unsigned int cmdValue);
-			void ir_decode(unsigned long code);
-			void ir_decode(byte *data);
-			void ir_to_bytes(unsigned long code, byte *result);
-			unsigned long bits_to_long(int *tmpBits);
-			void ir_dump(unsigned long code);
-			
-			unsigned int ir_recv_is_system;
-			unsigned int ir_recv_game_id;
-			unsigned int ir_recv_team_id;
-			unsigned int ir_recv_player_id;
-			unsigned int ir_recv_cmd;
-			unsigned int ir_recv_cmd_value;
-			unsigned int ir_recv_check_bit;
-			
-		private:
-	};
+  class Infinitag_Core
+  {
+    public:
+      Infinitag_Core();
+  
+      unsigned long irEncode(bool isSystem, unsigned int gameId, unsigned int teamId, unsigned int playerId, unsigned int cmd, unsigned int cmdValue);
+      void irDecode(unsigned long code);
+      void irDecode(byte *data);
+      void irToBytes(unsigned long code, byte *result);
+      unsigned long bitsToLong(int *tmpBits);
+      void irDump(unsigned long code);
+      
+      unsigned int irRecvIsSystem;
+      unsigned int irRecvGameId;
+      unsigned int irRecvTeamId;
+      unsigned int irRecvPlayerId;
+      unsigned int irRecvCmd;
+      unsigned int irRecvCmdValue;
+      unsigned int irRecvCheckBit;
+      
+    private:
+  };
 
 #endif
